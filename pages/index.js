e8695f006012ff0500e52465a69243b3b348d6a6
sisi95 <sjoukjeijlstra@gmail.com>
2021-02-17 13:43:07 +0100

    Added CSS for colour scheme. Removed duplicates. Sorted by date.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,15 @@ import styles from '../styles/Home.module.css';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 export default function Home({ upcomingLaunches, pastLaunches }) {
+
+  pastLaunches = pastLaunches.filter((v,i,a) => a.findIndex(t => (t.id === v.id)) === i).sort((a, b) => { 
+    return new Date(b.launch_date_local) - new Date(a.launch_date_local); 
+  });
+
+  upcomingLaunches = upcomingLaunches.filter((v,i,a) => a.findIndex(t => (t.id === v.id)) === i).sort((a, b) => {
+    return new Date(b.launch_date_local) - new Date(a.launch_date_local);
+  });
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,13 +25,13 @@ export default function Home({ upcomingLaunches, pastLaunches }) {
 
         <h2>Upcoming Launches</h2>
         <div className={styles.grid}>
-          {upcomingLaunches.sort((a, b) => { 
-            return new Date(b.launch_date_local) - new Date(a.launch_date_local); 
-          }).map((launch, key) => {
+          {upcomingLaunches.map((launch, key) => {
             return (
               <a key={key} className={styles.card}>
                 <h3>{ launch.mission_name}</h3>
                 <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("en-US") }</p>
+                <p><strong>Rocket Name:</strong> { launch.rocket.rocket_name }</p>
+                <p><strong>Launch Site:</strong> { launch.launch_site.site_name_long }</p>
               </a>
             );
           })}
@@ -30,14 +39,14 @@ export default function Home({ upcomingLaunches, pastLaunches }) {
 
         <h2>Past Launches</h2>
         <div className={styles.grid}>
-          {pastLaunches.sort((a, b) => { 
-            return new Date(b.launch_date_local) - new Date(a.launch_date_local); 
-          }).map((launch, key) => {
+          {pastLaunches.map((launch, key) => {
             return (
               <a key={key} href={launch.links.video_link} className={styles.card}>
                 <h3>{ launch.mission_name }</h3>
                 <p><strong>Launch Date:</strong> { new Date(launch.launch_date_local).toLocaleDateString("en-US") }</p>
-                {launch.launch_success ? <div>Launch succeeded</div> : <div>Launch failed</div>}
+                <p><strong>Rocket Name:</strong> { launch.rocket.rocket_name }</p>
+                <p><strong>Launch Site:</strong> { launch.launch_site.site_name_long }</p>
+                {launch.launch_success ? <p className={styles.statusSuccess}>Launch succeeded</p> : <p className={styles.statusFailure}>Launch failed</p>}
               </a>
             );
           })}
